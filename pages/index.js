8146188ba67315fa9e7535e3f50b3df28a4fae4a
yamagutikezya-cell <yamagutikezya@gmail.com>
2026-05-04 23:16:22 +0000

    adiciona scripts lint:check e lint:fix

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
-function Home(){
-    return <h1>If You sow this massage send me a sleeping emoji</h1>
+function Home() {
+  return <h1>If You sow this massage send me a sleeping emoji</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste() {
+  console.log("indetacao errada");
 }
 
 export default Home;
